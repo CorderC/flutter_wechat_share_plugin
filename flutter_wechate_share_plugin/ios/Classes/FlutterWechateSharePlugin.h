6bd4926ac01ca59019ffeea0716d323f253cb2fe
陈超 <chenchao01@sinoiov.com>
2021-05-19 17:24:11 +0800

    初始化组件项目工程

---
+++ flutter_wechate_share_plugin/ios/Classes/FlutterWechateSharePlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterWechateSharePlugin : NSObject<FlutterPlugin>
+@end
---
+++ flutter_wechate_share_plugin/ios/Classes/FlutterWechateSharePlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterWechateSharePlugin.h"
+
+@implementation FlutterWechateSharePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_wechate_share_plugin"
+            binaryMessenger:[registrar messenger]];
+  FlutterWechateSharePlugin* instance = [[FlutterWechateSharePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
