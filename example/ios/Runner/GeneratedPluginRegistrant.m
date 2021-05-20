ddb8c4a4fdfb86029959921604995445070904c7
陈超 <chenchao01@sinoiov.com>
2021-05-20 15:13:20 +0800

    完成license编写

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_wechate_share_plugin/FlutterWechateSharePlugin.h>)
+#import <flutter_wechate_share_plugin/FlutterWechateSharePlugin.h>
+#else
+@import flutter_wechate_share_plugin;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterWechateSharePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterWechateSharePlugin"]];
+}
+
+@end
